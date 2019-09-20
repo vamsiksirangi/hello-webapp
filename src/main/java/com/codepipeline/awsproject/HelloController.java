442c096724f72c2f2a13c9ce169896edf6aab343
Pavan Vamsi <vamsi.rhodes@gmail.com>
2019-09-20 15:34:21 -0500

    adding new project

---
+++ src/main/java/com/codepipeline/awsproject/HelloController.java
@@ -0,0 +1,20 @@
+package com.codepipeline.awsproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello.. This is my first pipeline";
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "Hello.. This is my first beanstalk instance";
+    }
+}
